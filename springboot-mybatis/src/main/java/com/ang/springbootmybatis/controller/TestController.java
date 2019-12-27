d3b473af630b10201d4565df532218ab1fa49c66
zifengye <122805311@qq.com>
2019-12-27 10:45:04 +0800

    集成druid数据连接池

--- springboot-mybatis/src/main/java/com/ang/springbootmybatis/controller/TestController.java
+++ springboot-mybatis/src/main/java/com/ang/springbootmybatis/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
@@ -14,6 +15,7 @@ public class TestController {
     TeacherMapper teacherMapper;
 
     @RequestMapping("/testInsert")
+    @ResponseBody
     public void testInsert(@RequestBody(required = false) Teacher teacher) {
 
         teacherMapper.insert(teacher);
